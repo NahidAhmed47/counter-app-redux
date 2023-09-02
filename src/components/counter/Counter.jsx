b0169f9e3de2f9a3b099d34c82ca368c81728c8c
NahidAhmed47 <nahidahmedsd47@gmail.com>
2023-09-02 16:48:33 +0600

    counter added when click

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,16 +1,21 @@
 "use client"
+import { decrement, increment } from "@/redux/slices/counterSlice";
 import React from "react";
+import { useSelector, useDispatch } from 'react-redux'
+
 
 const Counter = () => {
+    const count = useSelector((state) => state.counter.value)
+  const dispatch = useDispatch()
   return (
     <div className="text-center space-y-5">
       <h1 className="text-2xl font-semibold">Counter App By Redux</h1>
-      <p className="text-xl">0</p>
+      <p className="text-xl">{count}</p>
       <div className="flex gap-5">
-        <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+        <button onClick={() => dispatch(increment())} className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
           Increment
         </button>
-        <button className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
+        <button onClick={() => dispatch(decrement())} className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">
           Decrement
         </button>
       </div>
